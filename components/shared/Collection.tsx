42842b6543652649e48bd51d5288eaa94d88b9c9
rahmanabdur1 <rahmanabdurr65@gmail.com>
2024-03-22 06:43:39 -0700

    code added

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -61,7 +61,7 @@ export const Collection = ({
         </ul>
       ) : (
         <div className="collection-empty">
-          <p className="p-20-semibold">Empty List  . So, Go to Generative Fill</p>
+          <p className="p-20-semibold">Empty List . So, Go to Image Restore</p>
         </div>
       )}
 
